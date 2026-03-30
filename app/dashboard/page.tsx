9862f1b2a4acc3f16ab72a51d54274a5bd84149d
Sengly456 <senglyou84@gmail.com>
2026-03-30 11:15:43 +0700

    reconstruct mas callback url

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,9 +20,7 @@ export default async function DashboardPage() {
 
   const elementSsoUrl =
     process.env.NEXT_PUBLIC_ELEMENT_SSO_URL ||
-    `${normalizedElementBaseUrl}_matrix/client/v3/login/sso/redirect?redirectUrl=${encodeURIComponent(
-      normalizedElementBaseUrl,
-    )}`;
+    `${normalizedElementBaseUrl}upstream/authorize?provider=01JQ2Y5YJ7K4C8R9N6T3V1W2XA`;
 
   // Get Kratos session (includes OIDC provider logins like SimpleLogin)
   const session = await getServerSession();
